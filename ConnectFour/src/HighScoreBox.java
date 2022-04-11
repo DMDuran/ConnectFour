ffd763c5eca321d190e2d90dc3e0141258bcdb35
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-04-10 21:26:13 -0500

    High Score Box

---
+++ ConnectFour/src/HighScoreBox.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author marcoduran
+ *
+ */
+public class HighScoreBox {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
